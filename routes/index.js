a112c065227de620f9cf640ea1a2c1700241b0ee
Ilkka Huotari <ilkkah@gmail.com>
2015-02-23 06:31:39 +0000

    1st

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+	fb = require('ilkkah-fb'),
+	router = express.Router(),
+	appSecret = '7cd1fd1371b8792dbdc566ee40f62919';
+
+fb.options({ appSecret: appSecret });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/deauthorize', function(req, res, next) {
+	var request = fb.parseSignedRequest(req.body.signed_request, appSecret);
+	console.log('post /deauthorize', request);
+	res.send('');
+});
+
+router.get('/cb', function(req, res, next) {
+	console.log('get /cb', req.query);
+	if (req.query['hub.verify_token'] === 'moi') {
+		res.send(req.query['hub.challenge']);
+	}
+});
+
+router.post('/cb', function(req, res, next) {
+	console.log('post /cb', req.body);
+	res.send('');
+});
+
+module.exports = router;
